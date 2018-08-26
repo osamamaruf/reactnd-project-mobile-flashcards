c93a7d9d4e98b24fcaf563641755fccf9bacc27a
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-27 02:07:18 +0500

    refactor: renamed function

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,6 @@
 import { RECEIVE_DECKS, ADD_DECK, ADD_CARD } from '../actions'
 
-function entries (state = {}, action) {
+function decks (state = {}, action) {
     switch (action.type) {
       case RECEIVE_DECKS :
         return {
@@ -27,4 +27,4 @@ function entries (state = {}, action) {
     }
   }
   
-export default entries 
+export default decks 
