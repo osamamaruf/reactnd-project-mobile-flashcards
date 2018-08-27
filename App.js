7f362687f30c3234d8295334bbd4d2edda999580
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-27 20:11:46 +0500

    refactor: removed centered styles

--- App.js
+++ App.js
@@ -20,8 +20,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',    
   },
 });
