bf73e44aed652ab4008f8e1e2939f6f70e608c00
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-27 20:54:28 +0500

    feat: DeckList hooked up with data

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { receiveDecks } from '../actions'
+import { handleInitialData } from '../utils/api'
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class DeckList extends React.Component {
+class DeckList extends React.Component {
+  state = {      
+    ready : false
+  } 
+  
+  componentDidMount(){
+    const { dispatch } = this.props
+
+    handleInitialData()
+      .then((decks) => dispatch(receiveDecks(decks)))      
+      .then(() => this.setState(() => ({ready: true})))
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Deck List</Text>
+        <Text>{JSON.stringify(this.props.decks)}</Text>
       </View>
     );
   }
@@ -16,3 +31,11 @@ const styles = StyleSheet.create({
     flex: 1    
   },
 });
+
+function mapStateToProps (decks) {
+  return {
+    decks
+  }
+}
+
+export default connect(mapStateToProps)(DeckList)
