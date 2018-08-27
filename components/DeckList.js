0fe7b198323c37bd833c3e59db0c87bd6b6db15c
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-27 23:07:41 +0500

    feat: deck component incorporated in list view

--- components/DeckList.js
+++ components/DeckList.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { receiveDecks } from '../actions'
 import { handleInitialData } from '../utils/api'
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import Deck from './Deck'
 
 class DeckList extends React.Component {
   state = {      
@@ -17,10 +18,18 @@ class DeckList extends React.Component {
       .then(() => this.setState(() => ({ready: true})))
   }
 
+  renderItem = ({ item })=>{
+    return <Deck title={item.title} cardCount={item.questions.length}/>
+  }
+
   render() {
+    const { decks } = this.props
     return (
-      <View style={styles.container}>
-        <Text>{JSON.stringify(this.props.decks)}</Text>
+      <View style={styles.container}>  
+        <FlatList 
+          data={ Object.keys(decks).map((deck) => {return  decks[deck] })  }
+          renderItem={ this.renderItem }
+        />                   
       </View>
     );
   }
