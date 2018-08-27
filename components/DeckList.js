c2015a27ee094be873ce8309f95dc0198b667da7
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-28 00:25:31 +0500

    feat: decks made clickable

--- components/DeckList.js
+++ components/DeckList.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux'
 import { receiveDecks } from '../actions'
 import { handleInitialData } from '../utils/api'
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Platform } from 'react-native';
 import Deck from './Deck'
 import { AppLoading} from 'expo'
+import { white } from '../utils/colors'
 
 class DeckList extends React.Component { 
   state = {
@@ -19,7 +20,13 @@ class DeckList extends React.Component {
   }
 
   renderItem = ({ item })=>{
-    return <Deck title={item.title} cardCount={item.questions.length}/>
+    return (
+      <View style={styles.item}>
+        <TouchableOpacity onPress={() => console.log('Pressed!')}> 
+          <Deck title={item.title} cardCount={item.questions.length}/>
+        </TouchableOpacity>
+      </View>
+      )
   }
 
   render() {
@@ -44,6 +51,22 @@ class DeckList extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1    
+  }, 
+  item: {
+    backgroundColor: white,
+    borderRadius: Platform.OS === 'ios' ? 16 : 2,
+    padding: 20,
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 17,
+    justifyContent: 'center',
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowColor: 'rgba(0, 0, 0, 0.24)',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
   },
 });
 
