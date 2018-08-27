c52975dc2b911781bfa710cfe7deca21b1c3d244
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-27 20:30:40 +0500

    feat: basic view tab navigation added

--- App.js
+++ App.js
@@ -4,18 +4,35 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import DeckList from './components/DeckList'
+import NewDeck from './components/NewDeck'
+import { createMaterialTopTabNavigator, createStackNavigator } from 'react-navigation'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
-          <DeckList />
+          <Tabs />
         </View>
       </Provider>
     );
   }
 }
+
+const Tabs= createMaterialTopTabNavigator({
+  Decks: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Decks',      
+    },
+  },
+  NewDeck: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: 'New Deck',      
+    },
+  },
+});
 
 const styles = StyleSheet.create({
   container: {
