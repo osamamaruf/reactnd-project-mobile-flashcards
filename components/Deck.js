60f0f2f4ad85613cc49366486bda0cf182b50f23
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-27 23:07:23 +0500

    feat: Deck component title and questions shown

--- components/Deck.js
+++ components/Deck.js
@@ -3,9 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default class Deck extends React.Component {
   render() {
+    const { title, cardCount } = this.props
     return (
       <View style={styles.container}>
-        <Text>Deck</Text>
+        <Text>{ title }</Text>
+        <Text>{ cardCount } cards</Text>
       </View>
     );
   }
@@ -13,6 +15,9 @@ export default class Deck extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1    
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+
   },
 });
