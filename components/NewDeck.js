2f2c626dd75158d084fe4b5e78659c8f64995ee0
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-28 22:51:48 +0500

    feat: New deck screen functionality completed

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,18 +1,66 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, KeyboardAvoidingView, TextInput, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux'
+import { green, white, gray } from '../utils/colors'
 
-export default class NewDeck extends React.Component {
+class NewDeck extends React.Component {
+  state = {
+    title: ''
+  }
+
+  handleTextChange= (title) => {
+    console.log(title)
+    this.setState(()=> ({
+      title
+    }))
+  }
+  
   render() {
+    const { title } = this.state
     return (
-      <View style={styles.container}>
-        <Text>New Deck</Text>
-      </View>
+      <KeyboardAvoidingView behavior='padding' style={styles.container}>
+        <Text>What is the title of your new deck?</Text>
+        <TextInput
+          value={title}
+          style={styles.input}
+          onChangeText={this.handleTextChange}
+        />
+        <TouchableOpacity style={styles.btn} onPress={() => console.log(title)}>
+          <Text style={styles.btnText}>Submit</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1    
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',    
   },
+  btn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50 ,
+    marginTop: 17,
+    backgroundColor: green
+  },     
+  btnText:{
+    color: white
+  },
+  input:{
+    width: 200,
+    height: 44,
+    padding: 8,
+    margin: 50,
+    borderWidth: 1,
+    borderColor: gray
+
+  }
 });
+
+export default connect()(NewDeck)
