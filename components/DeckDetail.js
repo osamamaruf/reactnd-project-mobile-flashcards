7d92e05569fb763d268d4b8d18c50dde682fed73
Syed Osama Maruf <syedosamamaruf@gmail.com>
2018-08-28 21:48:40 +0500

    feat: Buttons and together with deck

--- components/DeckDetail.js
+++ components/DeckDetail.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux'
+import Deck from './Deck'
+import { white, red, green } from '../utils/colors'
 
-export default class DeckDetail extends React.Component {
+class DeckDetail extends React.Component {
   render() {
+    const { deck } = this.props 
     return (
       <View style={styles.container}>
-        <Text>Deck Detail- {JSON.stringify(this.props.navigation.state.params.key)}</Text>
+        <Deck title={deck.title} cardCount={deck.questions.length}/>
+        <TouchableOpacity style={[styles.btn, styles.addBtn]}>
+          <Text style={ styles.btnText }>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.btn, styles.quizBtn]}>
+          <Text style={ styles.btnText }>Start Quiz</Text>          
+        </TouchableOpacity>
       </View>
     );
   }
@@ -13,6 +23,34 @@ export default class DeckDetail extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1    
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',    
   },
+  btn: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+    padding: 10,
+    paddingLeft: 50,
+    paddingRight: 50   
+  },
+  addBtn: {
+    backgroundColor: green
+  },
+  quizBtn:{
+    backgroundColor: red
+  }, 
+  btnText:{
+    color: white
+  }
 });
+
+
+function mapStateToProps (decks, ownProps) {
+  return {
+    deck : decks[ownProps.navigation.state.params.key]
+  }
+}
+
+export default connect(mapStateToProps)(DeckDetail)
